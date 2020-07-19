119ab2d803581e8501a084d82572cd6baaf184ce
jenahiggs <jenahiggs@gmail.com>
2020-07-19 18:44:46 -0500

    UploadofHelloWorldapp
    Hello World app for VMware HW

---
+++ src/main/java/com/vmwarehw/helloworld/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.vmwarehw.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Hello World";
+		
+	}
+
+}
